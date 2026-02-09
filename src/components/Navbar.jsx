0dbad7e9da4f79d71d2c8659d9320107e91308f7
Mayank Pratap Singh <prmayanksingh05@gmail.com>
2026-02-09 18:25:23 +0530

    fix: correct backdrop styling in Navbar component for consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
             <h6 className='text-[.6em] md:text-[.7em] tracking-widest text-center font-[monument]'>STUDIO</h6>
         </div>
         <div className='flex items-center gap-[1em] md:gap-[1.5em]'>
-            <div className='bg-white/10 backdrop-blur-md h-fit px-[.9em] md:px-[.8em] py-[.6em] md:py-[.4em] rounded-full'><i className="ri-lightbulb-line text-[1.2em] md:text-[1.5em]"></i></div>
-            <h1 className='border-2 rounded-full px-[.7em] md:px-[.9em] py-[.4em] md:py-[.4em] text-[1.2em] md:text-[1.2em] font-semibold'><span className='hidden md:inline'>Say Hello</span> <i className="ri-arrow-right-up-line"></i></h1>
+            <div className='backdrop-blur-md h-fit px-[.9em] md:px-[.8em] py-[.6em] md:py-[.4em] rounded-full'><i className="ri-lightbulb-line text-[1.2em] md:text-[1.5em]"></i></div>
+            <h1 className='border-2 backdrop-blur-md rounded-full px-[.7em] md:px-[.9em] py-[.4em] md:py-[.4em] text-[1.2em] md:text-[1.2em] font-semibold'><span className='hidden md:inline'>Say Hello</span> <i className="ri-arrow-right-up-line"></i></h1>
             <div className='bg-white px-[.7em] md:px-[.7em] py-[.4em] md:py-[.4em] text-[1.2em] md:text-[1.3em] rounded-full'><i className="ri-menu-line text-black font-bold"></i></div>
         </div>
     </div>
