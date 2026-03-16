24af03806756de79867201efc5ebc13b316dea07
Mayank Pratap Singh <prmayanksingh05@gmail.com>
2026-03-16 13:18:19 +0530

    Add animations on multiple pages

--- src/sections/aboutus/CultureSection.jsx
+++ src/sections/aboutus/CultureSection.jsx
@@ -1,11 +1,37 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const CultureSection = ({img}) => {
+  const containerRef = useRef(null);
+  const imgRef = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.to(imgRef.current, {
+      yPercent: 12,
+      ease: "none",
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  }, []);
+
   return (
-    <div className="h-[45rem] sm:h-[50em] md:h-[55rem] xl:h-[65rem] w-full px-[2em] md:px-[3em] xl:px-[4em] py-[5em]">
-      <div className="h-full w-full bg-gray-500 rounded-4xl overflow-hidden">
+    <div className="h-[45rem] sm:h-[50em] md:h-[55rem] xl:h-[65rem] w-full px-[2em] md:px-[3em] xl:px-[4em] py-[8em]">
+      <div
+        ref={containerRef}
+        className="h-full w-full bg-gray-500 rounded-4xl overflow-hidden flex items-center justify-center"
+      >
         <img
-          className="h-full w-full object-cover scale-110"
+          ref={imgRef}
+          className="h-[120%] w-full object-cover"
           src={img}
-          alt="VR girl"
+          alt="Work Culture Image"
         />
       </div>
     </div>
